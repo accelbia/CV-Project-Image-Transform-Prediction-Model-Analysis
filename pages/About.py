b2cd2a92f1ba568494bf5ba496a7728bba1fa67f
accelbia <abex.official.cert@gmail.com>
2022-11-30 21:24:15 +0530

    Editing some names and forgotten mistakes :)

--- pages/About.py
+++ pages/About.py
@@ -4,7 +4,7 @@ from streamlit_lottie import st_lottie
 import pandas as pd
 st.set_page_config(layout="wide")
 
-st.title('Wavelet Transform Prediction Model Analysis - About')
+st.title('Image Transform Prediction Model Analysis - About')
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
